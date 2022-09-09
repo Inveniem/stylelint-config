c36048c66daeaede7103a97b1941006454f9219d
John Karahalis <john.karahalis@gmail.com>
2022-09-09 17:35:24 -0400

    Add support for SCSS

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 module.exports = {
   extends: ["stylelint-config-drupal", "stylelint-config-prettier"],
+  overrides: [
+    {
+      files: ["*.scss", "**/*.scss"],
+      extends: ["stylelint-config-standard-scss", "stylelint-config-prettier-scss"],
+      rules: {
+        // This rule finds false positives in multi-line comments that use // and
+        // have more than one paragraph...
+        //
+        // like this.
+        "scss/comment-no-empty": null,
+      }
+    },
+  ],
 };
