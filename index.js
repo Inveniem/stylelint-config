6bc513fb78429f8b6c89f3d625469e1b0b7cfc32
John Karahalis <john.karahalis@gmail.com>
2022-09-16 18:28:11 -0400

    Ensure Drupal rules take precedence for SCSS files
    This fixes an issue where some rules from stylelint-config-drupal were
    being overridden by rules from stylelint-config-standard-scss for SCSS
    files.

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ module.exports = {
   overrides: [
     {
       files: ["*.scss", "**/*.scss"],
+
+      // stylelint-config-standard-scss and stylelint-config-drupal disagree
+      // about some rules, so stylelint-config-drupal is extended after
+      // stylelint-config-standard-scss to ensure that its rules take
+      // precedence.
       extends: [
         "stylelint-config-standard-scss",
+        "stylelint-config-drupal",
         "stylelint-config-prettier-scss",
       ],
+
       rules: {
         // This rule finds false positives in multi-line comments that use // and
         // have more than one paragraph...
